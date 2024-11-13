d2d85d7baf5d2b1c095063ee269247dcad994a53
Константин <tractorx@yandex.ru>
2024-11-13 17:55:17 +0300

    Add files via upload

---
+++ module_9_5.py
@@ -0,0 +1,57 @@
+class StepValueError(ValueError):
+    pass
+
+class Iterator:
+    def __init__(self, start, stop, step=1):
+        self.pointer = 0
+        self.start = start
+        self.stop = stop
+        if step == 0:
+            raise StepValueError('шаг не может быть равен 0')
+        else:
+            self.step = step
+
+    def __iter__(self):
+        self.pointer = self.start
+        return self
+
+    def __next__(self):
+        if self.step > 0: # Случай, когда self.step = 0 мы изолировали на этапе создания объекта в __init__
+            if self.pointer <= self.stop:
+                self.pointer += self.step
+            else:
+                raise StopIteration
+        else: # Здесь рассмотрен случай, когда self.step < 0
+            if self.pointer >= self.stop:
+                self.pointer += self.step
+            else:
+                raise StopIteration
+
+        return self.pointer - self.step # Здесь мы убираем увеличение self.pointer на последнем шаге цикла
+
+
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
+
+iter2 = Iterator(-5, 1)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
+
+
+for i in iter2:
+    print(i, end=' ')
+print()
+for i in iter3:
+    print(i, end=' ')
+print()
+for i in iter4:
+    print(i, end=' ')
+print()
+for i in iter5:
+    print(i, end=' ')
+print()
